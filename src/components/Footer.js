1e5b1864e67521bc149217628c85f74a45beb10c
HugoLattard <hugolattard@gmail.com>
2020-08-30 22:47:18 +0200

    header responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import "../styles/styles.css";
 
 export default function Footer() {
   return (
-    <div className="footer red">
-      <div className="footer-container orange">
-        <h4>Michel Verjux</h4>
-      </div>
+    //     <div className="footer red">
+    <div className="footer-container">
+      <h4>Michel Verjux</h4>
+      {/* </div> */}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,36 +5,40 @@ import "../styles/styles.css";
 
 export default function Header() {
   return (
-    <div className="header red">
-      <div className="nav orange">
-        <ul className="logo-nav">
-          <li>
-            <Link to="/">
-              <h1 className="logo">Michel Verjux</h1>
-            </Link>
-          </li>
-          <li>
-            <span>Log in</span>
-          </li>
-        </ul>
-        <ul className="mini-nav">
-          <li>
-            <Link to="/image">
-              <span>Images</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/text">
-              <span>Textes</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/bio">
-              <span>Bio</span>
-            </Link>
-          </li>
-        </ul>
-      </div>
+    <div className="nav">
+      <ul className="logo-nav">
+        <li>
+          <Link to="/">
+            <h1 className="logo hover">Michel Verjux</h1>
+          </Link>
+        </li>
+        <li>
+          <span>Log in</span>
+        </li>
+        <li>
+          <select>
+            <option value="theme1">Mode sombre</option>
+            <option value="theme2">Mode clair</option>
+          </select>
+        </li>
+      </ul>
+      <ul className="mini-nav">
+        <li>
+          <Link to="/image">
+            <span className="hover">Images</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/text">
+            <span className="hover">Textes</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/bio">
+            <span className="hover">Bio</span>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
