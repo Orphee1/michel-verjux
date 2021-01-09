bddd227af6fe433e836cca2f7096075debc3e676
HugoLattard <hugolattard@gmail.com>
2021-01-09 17:01:12 +0100

    fix bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ const Header = ({
                 className="btn connect-btn"
                 onClick={() => {
                   //   logout({ returnTo: window.location.origin });
-                  logout({ returnTo: "https://michel-verjux.com/home," });
+                  logout({ returnTo: "https://michel-verjux.com/home" });
                 }}
                 // onClick={() => {
                 //   setUser({});
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.render(
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     //     redirectUri={window.location.origin}
-    redirectUri="https://michel-verjux.com/home,"
+    redirectUri="https://michel-verjux.com/home"
     cacheLocation="localstorage"
   >
     <UserProvider>
