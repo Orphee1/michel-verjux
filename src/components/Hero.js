1c667182340da366a67ea4b4b70ff90f8a72bd53
HugoLattard <hugolattard@gmail.com>
2021-01-13 16:26:29 +0100

    Added blur effect to preload landing an hero img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,52 +1,63 @@
-import React from 'react'
-import "../main.css"
-import styled from "styled-components"
-import img from "../images/hero.png"; 
+import React from "react";
+import "../main.css";
+import styled from "styled-components";
+import img from "../images/hero.png";
+import { heroBase64, preloadImageWithPromise } from "../constants/base64";
 
 const Hero = () => {
-        return (
-                <Wrapper>
-                             <div className="section-center hero-center">
-<h1>Michel Verjux</h1>
-                        </div>
-                  
-                </Wrapper>
-        )
-}
+  const [loaded, setLoaded] = React.useState(false);
 
-export default Hero
+  React.useEffect(() => {
+    preloadImageWithPromise(img)
+      .then(() => {
+        setLoaded(true);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const src = loaded ? img : heroBase64;
+
+  return (
+    <Wrapper style={{ backgroundImage: `url(${src})` }}>
+      <div className="section-center hero-center">
+        <h1>Michel Verjux</h1>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Hero;
 
 const Wrapper = styled.section`
-height: 90vh; 
- display: grid;
+  height: 90vh;
+  display: grid;
   place-items: center;
-background-image: url(${img}); 
-background-size: cover; 
-background-position: center;
+  /* background-image: url(${img}); */
 
-.hero-center {
-height: 85%; 
-display: grid; 
-grid-template-rows: 50% 50%; 
-h1 {
-  grid-row: 2/3; 
-  color: var(--clr-white); 
-  align-self: end ; 
-  justify-self: start; 
-  /* font-weight: bold;  */
-  
-}
-}
-@media screen and (min-width: 800px) {
-        .hero-center {
-                height: 90%;
-   grid-template-columns: 50% 50%; 
-   h1 {
-  grid-column: 1/3; 
-  grid-row: 2/3; 
-  align-self: end; 
-}
-        }
-}
+  background-size: cover;
+  background-position: center;
 
-`
+  .hero-center {
+    height: 85%;
+    display: grid;
+    grid-template-rows: 50% 50%;
+    h1 {
+      grid-row: 2/3;
+      color: var(--clr-white);
+      align-self: end;
+      justify-self: start;
+      /* font-weight: bold;  */
+    }
+  }
+  @media screen and (min-width: 800px) {
+    .hero-center {
+      height: 90%;
+      grid-template-columns: 50% 50%;
+      h1 {
+        grid-column: 1/3;
+        grid-row: 2/3;
+        align-self: end;
+      }
+    }
+  }
+`;
--- src/containers/LandingScreen.js
+++ src/containers/LandingScreen.js
@@ -1,26 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {SEO} from "../components";
-import img from "../images/landing-large.png"
+import { SEO } from "../components";
+import img from "../images/landing-large.png";
+import { landingBase64, preloadImageWithPromise } from "../constants/base64";
 import "../main.css";
-import styled from "styled-components"; 
+import styled from "styled-components";
 
 const LandingScreen = () => {
-  return (
-<Wrapper>
-<Link to="/home" >
-        <section className="landing-container">
-<div className="section-center landing-center">
-        <h1>Michel Verjux</h1>
-        <p>Isabelle Lartault et Michel Verjux, <i>Tout le reste est dans l'ombre</i>, Nuit Blanche, Paris, 2010. &copy; André Morin.</p>
-</div>
-        </section>
-</Link>
-</Wrapper>  
-  );
-}
+  const [loaded, setLoaded] = React.useState(false);
 
-export default LandingScreen
+  React.useEffect(() => {
+    preloadImageWithPromise(img)
+      .then(() => {
+        setLoaded(true);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const src = loaded ? img : landingBase64;
+
+  return (
+    <Wrapper style={{ backgroundImage: `url(${src})` }}>
+      <Link to="/home">
+        <section className="landing-container">
+          <div className="section-center landing-center">
+            <h1>Michel Verjux</h1>
+            <p>
+              Isabelle Lartault et Michel Verjux,{" "}
+              <i>Tout le reste est dans l'ombre</i>, Nuit Blanche, Paris, 2010.
+              &copy; André Morin.
+            </p>
+          </div>
+        </section>
+      </Link>
+    </Wrapper>
+  );
+};
+
+export default LandingScreen;
 
 const Wrapper = styled.main`
   position: fixed;
@@ -29,52 +46,48 @@ const Wrapper = styled.main`
   width: 100%;
   height: 100%;
   z-index: 999;
-background-image: url(${img});
-background-size: cover;
-background-position: center;
-.landing-container {
-        height: 100vh;
-        width: 100vw;
-}
-.landing-center {
-margin-top: 5rem;
-        height: 85%; 
-display: grid; 
-grid-template-rows: 50% 50%; 
-grid-template-columns: 50% 50%; 
-p {
-  grid-row: 2/3; 
-  color: var(--clr-white); 
-  place-self: end end;
-  width: 100px;  
-  font-size: 0.8rem; 
-  text-align: right; 
-
-}
-h1 {
-  grid-row: 2/3; 
-  color: var(--clr-white); 
-  align-self: start ; 
-  justify-self: start; 
-  
-}
-}
-@media screen and (min-width: 768px) {
-        .landing-center {
-   height: 90%;
-   grid-template-rows: 50% 50%; 
-h1 {
-  grid-column: 1/3; 
-  align-self: center; 
-}
-p {
-  grid-column: 2/3; 
-  grid-row: 2/3; 
-  width: 400px; 
-  align-self: end; 
-}
-}
-}
-`
-
-
+  /* background-image: url(${img}); */
+  background-size: cover;
+  background-position: center;
+  .landing-container {
+    height: 100vh;
+    width: 100vw;
+  }
+  .landing-center {
+    margin-top: 5rem;
+    height: 85%;
+    display: grid;
+    grid-template-rows: 50% 50%;
+    grid-template-columns: 50% 50%;
+    p {
+      grid-row: 2/3;
+      color: var(--clr-white);
+      place-self: end end;
+      width: 100px;
+      font-size: 0.8rem;
+      text-align: right;
+    }
+    h1 {
+      grid-row: 2/3;
+      color: var(--clr-white);
+      align-self: start;
+      justify-self: start;
+    }
+  }
+  @media screen and (min-width: 768px) {
+    .landing-center {
+      height: 90%;
+      grid-template-rows: 50% 50%;
+      h1 {
+        grid-column: 1/3;
+        align-self: center;
+      }
+      p {
+        grid-column: 2/3;
+        grid-row: 2/3;
+        width: 400px;
+        align-self: end;
+      }
+    }
+  }
+`;
